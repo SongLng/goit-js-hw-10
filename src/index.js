e599589af46e1b3a8bb9739a00c8848a19f0e546
Sergio Pieshko <sergio.pieshko@icloud.com>
2022-04-10 19:24:50 +0300

    try

--- src/index.js
+++ src/index.js
@@ -1,1 +1,78 @@
-import './sass/main.scss';
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './api/fetchCountries.js';
+import debounce from 'lodash.debounce';
+
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(event) {
+  event.preventDefault;
+
+  const searchName = event.target.value.trim();
+
+  console.log(searchName);
+  fetchCountries(searchName)
+    .then(countries => {
+      if (countries.length >= 10) {
+        renderList(countries);
+        searchingInfo();
+        setTimeout(() => {
+          refs.countryList.textContent = '';
+        }, 2000);
+      } else if (countries.length === 1) {
+        renderInfo(countries);
+      } else {
+        refs.countryInfo.innerHTML = '';
+      }
+    })
+    .catch(onFetchError());
+}
+
+function renderList(countries) {
+  const markupList = countries
+    .map(({ name, flags }) => {
+      return ` <li>
+      <span class="flag__list"><img class="flag__mini" src="${flags.svg}" alt="img"></span>
+      <span class="country">${name.official}</span>
+    </li>`;
+    })
+    .join('');
+  refs.countryList.innerHTML = markupList;
+  return markupList;
+}
+
+function renderInfo(countries) {
+  const markupInfo = countries
+    .map(({ capital, population, languages, flags }) => {
+      return ` <p class="country"><span class="flag"><img class="flag__info" src="${
+        flags.svg
+      }" alt="img"></span></p>
+        <p class="capital">Capital: ${capital}</p>
+        <p class="population">Population: ${population}</p>
+        <p class="languages">Languages: ${Object.values(languages).join(', ')}</p> `;
+    })
+    .join('');
+
+  refs.countryInfo.innerHTML = markupInfo;
+  return markupInfo;
+}
+
+function searchingInfo() {
+  setTimeout(() => {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  }, 2000);
+}
+
+function onFetchError() {
+  setTimeout(() => {
+    Notify.failure('Oops, there is no country with that name');
+  }, 5000);
+}
